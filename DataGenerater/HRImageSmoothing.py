936f8e208b6782ee3caebff3d23aea6a8ac10829
piratepanther <18637377877@163.com>
2020-12-05 10:07:27 +0800

    HRImageSmoothing.py

--- DataGenerater/HRImageSmoothing.py
+++ DataGenerater/HRImageSmoothing.py
@@ -3,12 +3,21 @@ import numpy as np
 import time
 
 #Variables
-ImageName="2020-12-02-18-27-35"
-
-
+ImageName="2020-12-02-18-30-51"
+ResizeWight=15000
+ResizeHeightScale=5.5
 #ProcessingRoad
 img = cv2.imread("20201202/HRimage/"+ImageName+".jpg", cv2.IMREAD_UNCHANGED)
+
+Height, Wight = img.shape[0:2]
+
+ResizeHeight=Height*ResizeHeightScale
+# Height= img.shape[0]
+img_Resize = cv2.resize(img, (int(ResizeWight), int(ResizeHeight)),interpolation=cv2.INTER_LINEAR)
+
+
 cv2.imshow("Image", img)
+cv2.imshow("Img_Resize", img_Resize)
 
 # #GaussianBlur
 # img_gauss = cv2.GaussianBlur(img,(5,5),1)
